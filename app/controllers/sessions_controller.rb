2f963eee5e2cbbb40b8aff9a6afe34cd71e53c6a
mono-glyceride <glyceride.mono@gmail.com>
2022-02-17 05:07:15 +0000

    update：値が有効な場合のログイン処理

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #ユーザーがデータベースにあり、かつ、認証に成功した
+      log_in user
+      redirect_to user
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
       flash.now[:danger] = 'Invalid email/password combination' 
