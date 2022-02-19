543aea01f2f2914925a328c283dbcf9352766751
mono-glyceride <glyceride.mono@gmail.com>
2022-02-19 09:04:21 +0000

    fix：パスワードを入れないとユーザーを編集できないバグを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-   def update
+  def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
