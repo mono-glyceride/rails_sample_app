21b8c7c41b69019b4d52ab0c0e71532bcc113aa1
mono-glyceride <glyceride.mono@gmail.com>
2022-02-19 13:19:26 +0000

    update：ユーザーの編集時にログインを要求する仕様に更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -44,4 +45,14 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
   
+  
+  # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
