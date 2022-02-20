eb79001ebf73007ef9e3da748f1dadb3faee0d98
mono-glyceride <glyceride.mono@gmail.com>
2022-02-20 10:19:02 +0000

    add：ユーザーの一覧を作成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,8 @@ module UsersHelper
   
   # 引数で与えられたユーザーのGravatar画像を返す
   def gravatar_for(user,options = { size: 80 })
+    size         = options[:size]
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
